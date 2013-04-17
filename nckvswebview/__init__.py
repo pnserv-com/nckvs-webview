350a7a5af6019ce0aac94e514c8d76317ef0afb8
Yoshihisa Tanaka <yoshihisa@iij.ad.jp>
2013-04-17 13:05:22 +0900

    add prototype

---
+++ nckvswebview/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from bottle import route, run
+
+from nckvsclient import KVSClient
+
+_clients = {}
+
+
+def get_client(datatype):
+    if datatype not in _clients:
+        client = KVSClient.from_file('nckvs-webview.ini')
+        client.config['datatypename'] = datatype
+        _clients[datatype] = client
+
+    return _clients[datatype]
+
+
+@route('/:datatype')
+def search(datatype):
+    client = get_client(datatype)
+    return client.search([])
+
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=9999)
